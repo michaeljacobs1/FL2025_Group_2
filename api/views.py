0a6eb8337d52338a6589da4f17505e9ac173c71e
Peter Adams-Agresti <a.peter@wustl.edu>
2025-09-16 18:38:05 -0500

    Trying to add Linter + Ruff

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import permissions, viewsets
+
 from .models import Post
 from .serializers import PostSerializer
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
     permission_classes = [permissions.AllowAny]  # dev: open API
+
 
 # Create your views here.
--- backend/settings.py
+++ backend/settings.py
@@ -37,11 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     # third-party apps
     "rest_framework",
     "corsheaders",
-
     # your app
     "api",
 ]
