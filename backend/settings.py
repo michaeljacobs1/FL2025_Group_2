da5ddcbfe130b920c942ced8df2d6366ba2d2353
michaeljacobs1 <michaelpjacobs1@gmail.com>
2025-09-25 18:41:53 -0500

    mysql added

--- backend/settings.py
+++ backend/settings.py
@@ -83,10 +83,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "appdb",
+        "USER": "appuser",
+        "PASSWORD": "strong-password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
 
